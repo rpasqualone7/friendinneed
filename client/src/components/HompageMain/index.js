a16c98f7b7a96e7c91462e33e948e02e4d821b81
Jessica Elias <jm.elias1990@yahoo.com>
2019-06-23 14:39:22 -0400

    Jessica

--- client/src/components/HompageMain/index.js
+++ client/src/components/HompageMain/index.js
@@ -1,23 +1,39 @@
 import React from 'react';
-import background from './background.jpg';
+import { Map, GoogleApiWrapper } from 'google-maps-react';
+
 
 
 function HomepageMain(props){
  return (
-<div>
-<image src={background} />
-<div className="section white">
-<div className="row container">
-<h2 className="header">
-A Friend in Need
-</h2>
-<p className="grey-text text-darken-3 lighten-3">
-You don't have to go through lifes struggles alone. You will always have a friend when you are in need.
-</p>
+<div class="row valign-wrapper card-panel teal lighten-3">
+    <div class="col s12 m7">
+        <div class="card">
+      <div class="card">
+        <div class="card-content center">
+          <span class="card-title red-text text-lighten-2">Never feel like you have to go it alone.</span>
+          <p>You don't have to go through lifes struggles alone. You will always have a friend when you are in need.</p>
+        </div>
+        <div class="card-action">
+          <a href="#">Register</a>
+          <a href="#">Sign in</a>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
-</div>
-</div>
+
+<div>       
+     <Map
+          google={this.props.google}
+          zoom={8}
+          style={mapStyles}
+          initialCenter={{ lat: 47.444, lng: -122.176}}
+        />
+        </div>
+
  );
 };
-
 export default HomepageMain;
+export default GoogleApiWrapper({
+  apiKey: 'TOKEN HERE'
+})(MapContainer);
