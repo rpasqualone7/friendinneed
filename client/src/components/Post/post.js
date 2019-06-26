eb8de2be18c32ec40da269e19014593749458875
rpasqualone7 <highwater92@gmail.com>
2019-06-26 14:36:11 -0400

    post editor fixed

--- client/src/components/Post/post.js
+++ client/src/components/Post/post.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Post = (props) => (
 	<div className='card post post-body'>
-          <div className='card-content'>
-            {props.postBody}
+          <div className='card-content' key={props.key}>
+            {props.post}
           </div>
         </div>
 );
-export default Post;
+export default Post; 
