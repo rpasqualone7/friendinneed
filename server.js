e8610b897d08d519e6792ca7bb929ce147184c20
rpasqualone7 <highwater92@gmail.com>
2019-07-02 22:06:17 -0400

    <final final4.OH NOOO>

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/friendinneed");
 app.use(routes);
 // Send every other request to the React app
 // Define any API routes before this runs
-app.get("/", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/public/index.html"));
 });
 
