f5b03036ad9547314c89e03912afa67d7f69249c
rpasqualone7 <highwater92@gmail.com>
2019-07-03 01:01:26 -0400

    <re-install>

--- routes/API/user.js
+++ routes/API/user.js
@@ -1,10 +1,10 @@
 const router = require ("express").Router();
 const controller = require("../../controllers/controller");
 
-// router.route("/")
-// .post(controller.createUser)
+router.route("/")
+.post(controller.createUser)
 
-// router.route("/:id")
-// .post(controller.findUserById)
+router.route("/:id")
+.post(controller.findUserById)
 
 module.exports = router;
